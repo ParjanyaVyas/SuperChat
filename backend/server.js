fd3c8e7d3aeabdc4a374e643ba8c0ea0e8ddf47c
Parjanya Vyas <90205885+ParjanyaVyas@users.noreply.github.com>
2023-12-28 18:46:14 +0530

    Update origin in server.js

--- backend/server.js
+++ backend/server.js
@@ -53,7 +53,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://superchat-c6g4.onrender.com/",
     // credentials: true,
   },
 });
